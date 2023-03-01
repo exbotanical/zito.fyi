154ad4875ce6f6461825f3092126c5a4e95a4bbb
Matthew Zito <exbotanical@protonmail.com>
2023-03-01 09:12:34 -0600

    fix: remove share box to prevent horizontal overflow on mobile viewports

--- src/components/Post/Share/styles.tsx
+++ src/components/Post/Share/styles.tsx
@@ -3,20 +3,13 @@ import styled from 'styled-components'
 
 import { H1 } from '@/styles'
 
-const makeBox = (color: string) =>
-  `data:image/svg+xml;utf8, <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 625 320.03" stroke="${color}" preserveAspectRatio="none"><path fill="none" stroke="${color}" stroke-miterlimit="10" stroke-width="5px" d="M611,3H15A12,12,0,0,0,3,15V253a12,12,0,0,0,12,12H381.49l65.64,50.45V265H611a12,12,0,0,0,12-12V15A12,12,0,0,0,611,3Z"/></svg>`
-
 export const Wrapper = styled.section`
   display: flex;
   grid-auto-flow: row;
-  margin: auto 0;
+  margin: 1rem auto;
   justify-content: flex-start;
-  stroke: ${({ theme }) => (theme.dark ? '' : 'rgb(0, 0, 0)')};
-  padding: 2rem 6.25rem 12.5rem 2rem;
-  margin: 1rem;
-
-  background: url('${({ theme }) => makeBox(theme.colors.base)}') no-repeat top
-    center;
+  stroke: ${({ theme }) =>
+    theme.dark ? 'rgb(255, 255, 255)' : 'rgb(0, 0, 0)'};
 `
 
 export const LinkWrapper = styled.div`
